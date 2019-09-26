056365603c36f80432d803bfbb91e870c22cab42
Toleutai Bekzat <toleutaib@gmail.com>
2019-09-26 20:51:09 +0600

    Добавил класс Item со своими свойствами

---
+++ Homework/Homework/Item.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework
+{
+  class Item
+  {
+    public string title { get; set; }
+    public string link { get; set; }
+    public string description { get; set; }
+    public DateTime pubDate { get; set; }
+  }
+}
